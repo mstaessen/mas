ea12cbb525ed7e6c7c0d61aec9d83cb9b75a5472
Michiel Staessen <staessenmichiel@gmail.com>
2012-04-02 16:48:21 +0200

    Added contract net framework. to be done.

---
+++ project/src/main/java/project/classic/contractnet/PackageAgent.java
@@ -0,0 +1,73 @@
+package project.classic.contractnet;
+
+import project.common.packages.Package;
+import rinde.sim.core.SimulatorAPI;
+import rinde.sim.core.SimulatorUser;
+import rinde.sim.core.TickListener;
+import rinde.sim.core.graph.Point;
+import rinde.sim.core.model.communication.CommunicationAPI;
+import rinde.sim.core.model.communication.CommunicationUser;
+import rinde.sim.core.model.communication.Mailbox;
+import rinde.sim.core.model.communication.Message;
+
+public class PackageAgent implements TickListener, SimulatorUser, CommunicationUser {
+
+	private SimulatorAPI simulator;
+	private Package myPackage;
+	private CommunicationAPI communicationAPI;
+	private Mailbox mailbox;
+	private double radius;
+	private double reliability;
+	
+	
+	public PackageAgent(Package myPackage, double radius, double reliability){
+		this.myPackage = myPackage;
+		this.radius = radius;
+		this.reliability = reliability;
+		this.mailbox = new Mailbox();
+	}
+	
+	@Override
+	public void setSimulator(SimulatorAPI api) {
+		this.simulator = api;
+	}
+
+	@Override
+	public void tick(long currentTime, long timeStep) {
+		//TODO exercise
+		
+	}
+
+	@Override
+	public void afterTick(long currentTime, long timeStep) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCommunicationAPI(CommunicationAPI api) {
+		this.communicationAPI = api;
+	}
+
+	@Override
+	public Point getPosition() {
+		//TODO
+		return this.myPackage.getPickupLocation();
+	}
+
+	@Override
+	public double getRadius() {
+		return radius;
+	}
+
+	@Override
+	public double getReliability() {
+		return reliability;
+	}
+
+	@Override
+	public void receive(Message message) {
+		this.mailbox.receive(message);
+	}
+
+}
